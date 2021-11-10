9d0ad122d275d398b7cea500ab6250d9e6093824
lin <9361471+vlinxh@users.noreply.github.com>
2021-11-10 16:07:53 +0800

    feat:add registry yapi frp koala-osmagic-project-qbj _koala-hitech-cloud

---
+++ deploy_yapi_compose/build/mongo/mongo-init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mongo -- "${MONGO_INITDB_DATABASE}" <<EOF
+db=db.getSiblingDB("admin")
+db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
+db=db.getSiblingDB("${MONGO_INITDB_DATABASE}")
+db.createUser({
+  user: "${MONGO_USERNAME}",
+  pwd: "${MONGO_PASSWORD}",
+  roles: [
+    { role: "dbAdmin", db: "${MONGO_INITDB_DATABASE}" },
+    { role: "readWrite", db: "${MONGO_INITDB_DATABASE}" }
+  ]
+})
+EOF
